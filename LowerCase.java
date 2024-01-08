6c29626e946c162f58873ccf246826badaaae640
Yarden Dadon <yarden.dadon@post.runi.ac.il>
2024-01-08 03:10:44 +0200

    Testin

--- LowerCase.java
+++ LowerCase.java
@@ -4,7 +4,7 @@ public class LowerCase {
         String str = args[0];
         System.out.println(lowerCase(str));
     }
-
+test
    /**
     * Returns a string which is identical to the original string, 
     * except that all the upper-case letters are converted to lower-case letters.
