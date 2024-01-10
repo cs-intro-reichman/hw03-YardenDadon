523db6485e0b4dc208d38906a96ae32c8dd57e44
Yarden Dadon <yarden.dadon@post.runi.ac.il>
2024-01-11 01:28:00 +0200

    Calander pt somtheing

--- Calendar1.java
+++ Calendar1.java
@@ -9,13 +9,23 @@ public class Calendar1 {
 	static int dayOfWeek = 2;     // 1.1.1900 was a Monday
 	static int nDaysInMonth = 31; // Number of days in January
 	public static void main(String args[]) {
+		// Advances the date and the day-of-the-week from 1/1/1900 till 31/12/1999, inclusive.
+	    // Prints each date dd/mm/yyyy in a separate line. If the day is a Sunday, prints "Sunday".
+	    // The following variable, used for debugging purposes, counts how many days were advanced so far.
+	    //// Write the necessary initialization code, and replace the condition
+	    //// of the while loop with the necessary condition 
 		int sundayCounter = 0;
 	 	while (year != 2000) {
-	 		System.out.print(dayOfMonth + "/" + month + "/" + year);
-			 
-	 		 advance();
-	 		if (false)
-        }
+	 		if (dayOfWeek == 1){
+				System.out.println(dayOfMonth + "/" + month + "/" + year + " Sunday");
+				if (dayOfMonth == 1) {
+					sundayCounter++;
+				}
+			}else {
+				System.out.println(dayOfMonth + "/" + month + "/" + year);
+			}
+			advance();		
+		}
 		System.out.println("During the 20th century, " + sundayCounter + " Sundays fell on the first day of the month");
 	 }
 	
@@ -23,12 +33,20 @@ public class Calendar1 {
 	 // If the month changes, sets the number of days in this month.
 	 // Side effects: changes the static variables dayOfMonth, month, year, dayOfWeek, nDaysInMonth.
 	 private static void advance() {
-		
-		
+		dayOfMonth++;
+		dayOfWeek++;
+		if (dayOfMonth > nDaysInMonth(month, year)){
+			dayOfMonth = 1; 
+			month++;
+		}
+		if (month > 12){
+			month = 1; 
+			year++;
+		}
+		if (dayOfWeek > 7){
+			dayOfWeek = 1;
+		}
 	}
-
-
-	 
 	private static boolean isLeapYear(int year) {
 		boolean leapTruthFalse;
 	    if (year % 4 == 0){ leapTruthFalse = true;}
@@ -39,57 +57,57 @@ public class Calendar1 {
 		int days = 0;
 		if (isLeapYear(year) == true){
 			switch(month){
-				case 0: days = 31;
+				case 1: days = 31;
 				break;
-				case 1: days = 29;
+				case 2: days = 29;
 				break;
-				case 2: days = 31;
+				case 3: days = 31;
 				break;
-				case 3: days = 30;
+				case 4: days = 30;
 				break;
-				case 4: days = 31;
+				case 5: days = 31;
 				break;
-				case 5: days = 30;
-				break;
-				case 6: days = 31;
+				case 6: days = 30;
 				break;
 				case 7: days = 31;
 				break;
-				case 8: days = 30;
+				case 8: days = 31;
 				break;
-				case 9: days = 31;
+				case 9: days = 30;
 				break;
-				case 10: days = 30;
+				case 10: days = 31;
 				break;
-				case 11: days = 31;
+				case 11: days = 30;
+				break;
+				case 12: days = 31;
 				break;
 			}
 		}
 		else {
 			switch(month){
-				case 0: days = 31;
+				case 1: days = 31;
 				break;
-				case 1: days = 28;
+				case 2: days = 28;
 				break;
-				case 2: days = 31;
+				case 3: days = 31;
 				break;
-				case 3: days = 30;
+				case 4: days = 30;
 				break;
-				case 4: days = 31;
+				case 5: days = 31;
 				break;
-				case 5: days = 30;
-				break;
-				case 6: days = 31;
+				case 6: days = 30;
 				break;
 				case 7: days = 31;
 				break;
-				case 8: days = 30;
+				case 8: days = 31;
 				break;
-				case 9: days = 31;
+				case 9: days = 30;
 				break;
-				case 10: days = 30;
+				case 10: days = 31;
 				break;
-				case 11: days = 31;
+				case 11: days = 30;
+				break;
+				case 12: days = 31;
 				break;
 			}
 		}		
